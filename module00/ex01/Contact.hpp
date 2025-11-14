e5560c9fdaf56e8481a52c337a3744172aaec5fe
Mohammed Abrar Shariar <mshariar@k1r1p11.42mulhouse.fr>
2025-11-15 00:05:12 +0100

    fuck cpp

--- module00/ex01/Contact.hpp
+++ module00/ex01/Contact.hpp
@@ -6,7 +6,38 @@
 /*   By: mshariar <mshariar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 23:37:16 by mshariar          #+#    #+#             */
-/*   Updated: 2025/11/14 23:39:53 by mshariar         ###   ########.fr       */
+/*   Updated: 2025/11/15 00:03:52 by mshariar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact
+{
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+    
+public:
+    void setFirstName(std::string name);
+    void setLastName(std::string name);
+    void setNickname(std::string nick);
+    void setPhoneNumber(std::string phone);
+    void setDarkestSecret(std::string secret);
+
+    std::string getFirstName();
+    std::string getLastName();
+    std::string getNickname();
+    std::string getPhoneNumber();
+    std::string getDarkestSecret();
+};
+
+#endif
+
 
